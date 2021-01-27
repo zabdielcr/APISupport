0b241a6c42f76d15f46290752b7fef0e721720da
gloryaj <37586079+gloryaj@users.noreply.github.com>
2021-01-26 20:17:41 -0600

    Update dbSupport

--- dbSupport.sql
+++ dbSupport.sql
@@ -1,33 +1,40 @@
-USE [dbSupport]
+USE [Support]
 GO
 
-/****** Object:  Table [dbo].[Issue]    Script Date: 1/22/2021 12:30:21 AM ******/
-SET ANSI_NULLS ON
+CREATE TABLE [dbo].[Supporter](
+	[Id] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](30) NOT NULL,
+	[FirstName] [nvarchar](30) NOT NULL,
+	[SecondName] [nvarchar](30) NOT NULL,
+	[Address] [nvarchar](40) NOT NULL,
+	[Email] [nvarchar](40) NOT NULL,
+	[Password] [nvarchar](40) NOT NULL,
+	[role] [int] NOT NULL,
+ )
+
+ CREATE TABLE [dbo].[Supervisor](
+	[Id] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](30) NOT NULL,
+	[FirstName] [nvarchar](30) NOT NULL,
+	[SecondName] [nvarchar](30) NOT NULL,
+	[Address] [nvarchar](40) NOT NULL,
+	[Email] [nvarchar](40) NOT NULL,
+	[Password] [nvarchar](40) NOT NULL,
+	[role] [int] NOT NULL,
+)
 GO
 
-SET QUOTED_IDENTIFIER ON
-GO
+Create table Issue(
+	ReportNumber int primary key identity(1,1),
+	IdClient int,
+	[Description] varchar(500),
+	time_stamp date,
+	contact_phone varchar(255),
+	contact_email varchar(255),
+	[status] varchar(255),
+	[name] varchar(255)
+)
 
-CREATE TABLE [dbo].[Issue](
-	[ReportNumber] [int] IDENTITY(1,1) NOT NULL,
-	[Classification] [nvarchar](30) NOT NULL,
-	[Status] [nvarchar](30) NOT NULL,
-	[ReportTimestamp] [timestamp] NOT NULL,
-	[ResolutionComment] [nvarchar](50) NOT NULL,
-	[IdSupporter] [nvarchar](40) NOT NULL,
- CONSTRAINT [PK_Issue] PRIMARY KEY CLUSTERED 
-(
-	[ReportNumber] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-
-ALTER TABLE [dbo].[Issue]  WITH CHECK ADD  CONSTRAINT [FK_Issue_Supporter] FOREIGN KEY([IdSupporter])
-REFERENCES [dbo].[Supporter] ([Email])
-GO
-
-ALTER TABLE [dbo].[Issue] CHECK CONSTRAINT [FK_Issue_Supporter]
-GO
 
 CREATE TABLE [dbo].[Note](
 	[IdNotes] [int] IDENTITY(1,1) NOT NULL,
@@ -74,30 +81,9 @@ GO
 ALTER TABLE [dbo].[Service] CHECK CONSTRAINT [FK_Service_Supporter]
 GO
 
-CREATE TABLE [dbo].[Supervisor](
-	[Name] [nvarchar](30) NOT NULL,
-	[FirtsSurname] [nvarchar](30) NOT NULL,
-	[SecondSurname] [nvarchar](30) NOT NULL,
-	[Email] [nvarchar](40) NOT NULL,
- CONSTRAINT [PK_Supervisor] PRIMARY KEY CLUSTERED 
-(
-	[Email] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
 
-CREATE TABLE [dbo].[Supporter](
-	[Name] [nvarchar](30) NOT NULL,
-	[FirstSurname] [nvarchar](30) NOT NULL,
-	[SecondSurname] [nvarchar](30) NOT NULL,
-	[Email] [nvarchar](40) NOT NULL,
-	[IdSupervisor] [nvarchar](40) NOT NULL,
- CONSTRAINT [PK_Supporter] PRIMARY KEY CLUSTERED 
-(
-	[Email] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+
+
 
 ALTER TABLE [dbo].[Supporter]  WITH CHECK ADD  CONSTRAINT [FK_Supporter_Supervisor] FOREIGN KEY([IdSupervisor])
 REFERENCES [dbo].[Supervisor] ([Email])
